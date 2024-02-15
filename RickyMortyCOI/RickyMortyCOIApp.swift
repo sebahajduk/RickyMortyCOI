74985c3da797280fa52682428a59d5c5a428c540
Sebastian Hajduk <jiuzeer@gmail.com>
2024-02-15 20:12:48 +0100

    initial commit

---
+++ RickyMortyCOI/RickyMortyCOIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickyMortyCOIApp.swift
+//  RickyMortyCOI
+//
+//  Created by Sebastian Hajduk on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickyMortyCOIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
