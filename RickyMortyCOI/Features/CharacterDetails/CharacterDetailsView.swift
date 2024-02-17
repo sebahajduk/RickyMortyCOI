4dc750518a607bc2961fcbdffc350234c206590e
Sebastian Hajduk <jiuzeer@gmail.com>
2024-02-17 22:56:20 +0100

    git bug fix

---
+++ RickyMortyCOI/Features/CharacterDetails/CharacterDetailsView.swift
@@ -0,0 +1,140 @@
+//
+//  CharacterDetailsView.swift
+//  RickyMortyCOI
+//
+//  Created by Sebastian Hajduk on 15/02/2024.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct CharacterDetailsView: View {
+
+    @Perception.Bindable var store: StoreOf<CharacterDetailsReducer>
+
+    var body: some View {
+        WithPerceptionTracking {
+            NavigationView {
+                ZStack(alignment: .top) {
+                    Color.customBlack.ignoresSafeArea()
+
+                    VStack(spacing: 10.0) {
+                        AsyncImage(url: URL(string: store.character.image)) { image in
+                            image.resizable()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(maxWidth: .infinity)
+                        .aspectRatio(contentMode: .fill)
+                        .overlay {
+                            LinearGradient(
+                                colors: [
+                                    .customBlack,
+                                    .clear,
+                                    .clear
+                                ],
+                                startPoint: .bottom,
+                                endPoint: .top
+                            )
+                        }
+
+                        AsyncImage(url: URL(string: store.character.image)) { image in
+                            image.resizable()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(maxWidth: .infinity)
+                        .brightness(0.3)
+                        .blur(radius: 50.0)
+                        .mask {
+                            characterDetails
+                        }
+
+                        ScrollView {
+                            ForEach(store.character.episode, id: \.self) { episodeURL in
+                                AsyncImage(url: URL(string: store.character.image))
+                                    .frame(height: 30.0)
+                                    .brightness(0.3)
+                                    .blur(radius: 50.0)
+                                    .mask {
+                                        Text("Episode \(episodeURL.mapEpisodeURLToNumber())".uppercased())
+                                            .frame(maxWidth: .infinity)
+                                            .font(.system(size: 15.0, weight: .black))
+                                    }
+                                    .onTapGesture {
+                                        var transaction = Transaction()
+                                        transaction.disablesAnimations = true
+
+                                        store.send(.episodeTapped(episode: episodeURL))
+                                    }
+                            }
+                        }
+                    }
+                    .ignoresSafeArea()
+                }
+                .fullScreenCover(isPresented: $store.episodeDetailsIsPresented.sending(\.setEpisodeDetails)) {
+                    if let store = store.scope(state: \.episodeDetails, action: \.episodeDetails) {
+                        EpisodeDetailsView(
+                            store: store
+                        )
+                        .background(ClearBackground())
+                        .ignoresSafeArea()
+                    }
+                }
+            }
+            .navigationBarBackButtonHidden(store.episodeDetailsIsPresented)
+        }
+    }
+
+    var characterDetails: some View {
+        Group {
+            Text(store.character.name.uppercased())
+                .font(.system(size: 30.0, weight: .black))
+                .lineLimit(1)
+                .minimumScaleFactor(0.3)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10.0)
+                        .inset(by: -5.0)
+                        .stroke()
+                }
+
+            ParameterView(parameterName: "Status", parameterValue: store.character.status)
+                .padding(.top, 5.0)
+            ParameterView(parameterName: "Gender", parameterValue: store.character.gender)
+
+            ParameterView(parameterName: "Origin", parameterValue: store.character.origin.name)
+
+            ParameterView(parameterName: "Location", parameterValue: store.character.location.name)
+
+            Rectangle()
+                .frame(height: 1)
+                .padding(.vertical, 10.0)
+
+            Text("Episodes".uppercased())
+                .font(.system(size: 20.0, weight: .black))
+        }
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    CharacterDetailsView(
+        store: Store(
+            initialState: CharacterDetailsReducer.State(
+                character: Character(
+                    id: 1,
+                    name: "Ricky Morty",
+                    status: "Alive",
+                    gender: "Male",
+                    origin: Origin(name: "Earth"),
+                    location: LastKnownLocation(name: "Earth"),
+                    image: "https://rickandmortyapi.com/api/character/avatar/361.jpeg",
+                    episode: []
+                )
+            ),
+            reducer: {
+                CharacterDetailsReducer()
+            }
+        )
+    )
+}
---
+++ RickyMortyCOI/Features/CharactersList/CharactersReducer.swift
@@ -0,0 +1,64 @@
+//
+//  CharactersReducer.swift
+//  RickyMortyCOI
+//
+//  Created by Sebastian Hajduk on 15/02/2024.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+@Reducer
+struct CharactersReducer {
+    @ObservableState
+    struct State {
+        @Presents var characterDetails: CharacterDetailsReducer.State?
+
+        var characters: IdentifiedArrayOf<Character> = []
+        var showCharactersList = false
+        var page = 1
+    }
+
+    enum Action {
+        case showListButtonTapped
+        case charactersResponse([Character])
+        case reachedBottomOnList
+    }
+
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .showListButtonTapped:
+                withAnimation {
+                    state.showCharactersList.toggle()
+                }
+                
+                if state.showCharactersList {
+                    state.page = 1
+                    
+                    return .run { send in
+                        let charactersList = try await NetworkService.fetchCharactersList(for: 1)
+                        
+                        await send(.charactersResponse(charactersList))
+                    }
+                } else {
+                    state.characters = []
+                    return .none
+                }
+            case .charactersResponse(let response):
+                response.forEach { state.characters.append($0) }
+                
+                return .none
+            case .reachedBottomOnList:
+                let nextPage = state.page + 1
+                state.page = nextPage
+                
+                return .run { send in
+                    let charactersList = try await NetworkService.fetchCharactersList(for: nextPage)
+                    
+                    await send(.charactersResponse(charactersList))
+                }
+            }
+        }
+    }
+}
